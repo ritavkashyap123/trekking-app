842b6fa85aaea9087e0a4edba84327116d86d13f
Ritav Kashyap <ritav20-237@aec.ac.in>
2023-09-25 02:07:21 +0530

    minor update

--- trecking-app/src/signin-up/Sign.jsx
+++ trecking-app/src/signin-up/Sign.jsx
@@ -1,26 +1,35 @@
 import React, { useState } from "react";
-import "../App.css";
+import "./Sign.css";
 
 function Sign() {
-  const signUpButton = document.getElementById("signUp");
-  const signInButton = document.getElementById("signIn");
-  const container = document.getElementById("container");
+  // const signUpButton = document.getElementById("signUp");
+  // const signInButton = document.getElementById("signIn");
+  // const container = document.getElementById("container");
 
-  signUpButton.addEventListener("click", () =>
-    container.classList.add("right-panel-active")
-  );
+  // signUpButton.addEventListener("click", () =>
+  //   container.classList.add("right-panel-active")
+  // );
 
-  signInButton.addEventListener("click", () =>
-    container.classList.remove("right-panel-active")
-  );
+  // signInButton.addEventListener("click", () =>
+  //   container.classList.remove("right-panel-active")
+  // );
+
+  const [isSignUpActive, setIsSignUpActive] = useState(false);
+
+  const handleSignUpClick = () => {
+    setIsSignUpActive(true);
+  };
+
+  const handleSignInClick = () => {
+    setIsSignUpActive(false);
+  };
 
   return (
-    <>
-      <div className="container" id="container">
-        <div className="form-container sign-up-container">
-          <form action="#">
-            <h1>Create Account</h1>
-            {/* <div className="social-container">
+    <div className={`container ${isSignUpActive ? "right-panel-active" : ""}`}>
+      <div className="form-container sign-up-container">
+        <form action="#">
+          <h1>Create Account</h1>
+          {/* <div className="social-container">
               <a href="#" className="social">
                 <i className="fab fa-facebook-f" />
               </a>
@@ -31,18 +40,18 @@ function Sign() {
                 <i className="fab fa-linkedin-in" />
               </a>
             </div> */}
-            <span>or use your email for registration</span>
-            <input type="text" placeholder="Name" />
-            <input type="number" placeholder="Phone No" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <button style={{marginTop: 10}}>Sign Up</button>
-          </form>
-        </div>
-        <div className="form-container sign-in-container">
-          <form action="#">
-            <h1>Sign in</h1>
-            {/* <div className="social-container">
+          <span>or use your email for registration</span>
+          <input type="text" placeholder="Name" />
+          <input type="number" placeholder="Phone No" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button style={{ marginTop: 10 }}>Sign Up</button>
+        </form>
+      </div>
+      <div className="form-container sign-in-container">
+        <form action="#">
+          <h1>Sign in</h1>
+          {/* <div className="social-container">
               <a href="#" className="social">
                 <i className="fab fa-facebook-f" />
               </a>
@@ -53,35 +62,34 @@ function Sign() {
                 <i className="fab fa-linkedin-in" />
               </a>
             </div> */}
-            <span>or use your account</span>
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <a href="#">Forgot your password?</a>
-            <button>Sign In</button>
-          </form>
-        </div>
-        <div className="overlay-container">
-          <div className="overlay">
-            <div className="overlay-panel overlay-left">
-              <h1>Welcome Back!</h1>
-              <p>
-                To keep connected with us please login with your personal info
-              </p>
-              <button className="ghost" id="signIn">
-                Sign In
-              </button>
-            </div>
-            <div className="overlay-panel overlay-right">
-              <h1>Hello, Friend!</h1>
-              <p>Enter your personal details and start journey with us</p>
-              <button className="ghost" id="signUp">
-                Sign Up
-              </button>
-            </div>
+          <span>or use your account</span>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <a href="#">Forgot your password?</a>
+          <button>Sign In</button>
+        </form>
+      </div>
+      <div className="overlay-container">
+        <div className="overlay">
+          <div className="overlay-panel overlay-left">
+            <h1>Welcome Back!</h1>
+            <p>
+              To keep connected with us please login with your personal info
+            </p>
+            <button className="btn" id="signIn" onClick={handleSignInClick}>
+              Sign In
+            </button>
+          </div>
+          <div className="overlay-panel overlay-right">
+            <h1>Hello, Friend!</h1>
+            <p>Enter your personal details and start journey with us</p>
+            <button className="btn" id="signUp" onClick={handleSignUpClick}>
+              Sign Up
+            </button>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
