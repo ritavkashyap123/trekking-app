3c3f2b125ddc6ed0b8505b01adfb9a0fa95173ff
Ritav Kashyap <ritav20-237@aec.ac.in>
2023-10-10 18:24:52 +0530

    user page done

--- trecking-app/src/pages/track/Tracks.jsx
+++ trecking-app/src/pages/track/Tracks.jsx
@@ -1,110 +1,211 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import Box from "@mui/material/Box";
 
 import "./tracks.scss";
-import { Button } from "@mui/material";
 
-export const renderDetailsButton = (params) => {
-  return (
-      <strong>
-          <Button
-              variant="contained"
-              color="primary"
-              size="small"
-              style={{ marginLeft: 16 }}
-              onClick={() => {
-                  parseName(params.row.col6)
-              }}
-          >
-              More Info
-          </Button>
-      </strong>
-  )
-}
 
-const columns = [
-  { field: "id", headerName: "ID", width: 90 },
-  {
-    field: "firstName",
-    headerName: "First name",
-    width: 150,
-    editable: true,
-  },
-  {
-    field: "lastName",
-    headerName: "Last name",
-    width: 150,
-    editable: true,
-  },
-  {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 110,
-    editable: true,
-  },
-  {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    editable: true,
-  },
-  {
-    field: "edit",
-    headerName: "Edit",
-    sortable: false,
-    width: 160,
-    renderCell: renderDetailsButton,
-    editable: false,
-  },
-  {
-    field: "delete",
-    headerName: "Delete",
-    sortable: false,
-    width: 160,
-    editable: true,
-  },
-];
-
-const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35},
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-  { id: 10, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 11, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 12, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 13, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 14, lastName: "Roxie", firstName: "Harvey", age: 65 },
-  { id: 15, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 16, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 17, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 18, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 19, lastName: "Roxie", firstName: "Harvey", age: 65 },
-];
 
 function Tracks() {
+  const [track, setTrack] = useState([])
+  const columns = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "trackname",
+      headerName: "Track Name",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "date",
+      headerName: "Date",
+      type: "date",
+      width: 150,
+      editable: true,
+      valueGetter: (params) => {
+        const rawDateValue = params.row.date;
+        const dateObject = new Date(rawDateValue);
+        if (isNaN(dateObject.getTime())) {
+          return null;
+        }
+        return dateObject;
+      },
+    },
+    {
+      field: "participant",
+      headerName: "Participant",
+      type: "number",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "capacity",
+      headerName: "Capacity",
+      type: "number",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "edit",
+      headerName: "Edit",
+      sortable: false,
+      width: 160,
+      renderCell: (params) => {
+        const handleEditClick = () => {
+          const rowData = params.row;
+          console.log("Edit button clicked for row with ID:", rowData.id);
+
+          setTrack(rows )
+          // console.log(track)
+        };
+        return (
+          <div className="edit" onClick={handleEditClick}>
+            Edit
+          </div>
+        );
+      },
+      editable: false,
+    },
+    {
+      field: "delete",
+      headerName: "Delete",
+      sortable: false,
+      width: 160,
+      renderCell: (params) => {
+        const handleEditClick = () => {
+          const rowData = params.row;
+          console.log("Edit button clicked for row with ID:", rowData.id);
+        };
+        return (
+          <div className="delete" onClick={handleEditClick}>
+            Delete
+          </div>
+        );
+      },
+      editable: false,
+    },
+  ];
+  
+  const rows = [
+    {
+      id: 1,
+      trackname: "ABC",
+      date: "2015-03-25",
+      participant: 19,
+      capacity: 25,
+    },
+    {
+      id: 2,
+      trackname: "ABC",
+      date: "2015-03-25",
+      participant: 19,
+      capacity: 25,
+    },
+    {
+      id: 3,
+      trackname: "ABC",
+      date: "2015-03-25",
+      participant: 19,
+      capacity: 25,
+    },
+    {
+      id: 4,
+      trackname: "ABC",
+      date: "2015-03-25",
+      participant: 19,
+      capacity: 25,
+    },
+    {
+      id: 5,
+      trackname: "ABC",
+      date: "2015-03-25",
+      participant: 19,
+      capacity: 25,
+    },
+    {
+      id: 6,
+      trackname: "ABC",
+      date: "2015-03-25",
+      participant: 19,
+      capacity: 25,
+    },
+    {
+      id: 7,
+      trackname: "ABC",
+      date: "2015-03-25",
+      participant: 19,
+      capacity: 25,
+    },
+    {
+      id: 8,
+      trackname: "ABC",
+      date: "2015-03-25",
+      participant: 19,
+      capacity: 25,
+    },
+    {
+      id: 9,
+      trackname: "ABC",
+      date: "2015-03-25",
+      participant: 19,
+      capacity: 25,
+    },
+    {
+      id: 10,
+      trackname: "ABC",
+      date: "2015-03-25",
+      participant: 19,
+      capacity: 25,
+    },
+    {
+      id: 11,
+      trackname: "ABC",
+      date: "2015-03-25",
+      participant: 19,
+      capacity: 25,
+    },
+    {
+      id: 12,
+      trackname: "ABC",
+      date: "2015-03-25",
+      participant: 19,
+      capacity: 25,
+    },
+    {
+      id: 13,
+      trackname: "ABC",
+      date: "2015-03-25",
+      participant: 19,
+      capacity: 25,
+    },
+  ];
+  useEffect(() => {
+    setTrack(rows)
+  }, [])
+  
   return (
     <div className="Tracks">
+      <div className="top">
+        <div className="text">
+          <h2>All Tracks</h2>
+        </div>
+        <div className="btn">Add New</div>
+      </div>
+      {console.log(track)}
       <Box sx={{ height: "100%", width: "100%" }}>
         <DataGrid
-          rows={rows}
+          sx={{ fontFamily: "Kanit", display: "flex", justifyContent: "center" }}
+          rows={track}
           columns={columns}
-          initialState={{
-            pagination: {
-              paginationModel: {
-                pageSize: 14,
-              },
-            },
-          }}
+          // initialState={{
+          //   pagination: {
+          //     paginationModel: {
+          //       // pageSize: 14,
+          //     },
+          //   },
+          // }}
+          autoPageSize
+          {...rows}
           pageSizeOptions={[5]}
           checkboxSelection
           disableRowSelectionOnClick
